1e022f2db423a0d6a0f495ad36619d83c2636609
Nicolas Torrens <80142957+NMTorrens@users.noreply.github.com>
2025-06-18 16:41:04 -0400

    Initial Commit

---
+++ StockTracker/StockTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockTrackerApp.swift
+//  StockTracker
+//
+//  Created by Nicolas Torrens on 6/18/25.
+//
+
+import SwiftUI
+
+@main
+struct StockTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
